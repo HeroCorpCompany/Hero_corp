8139594a3a9c863ccf891aa64179d81c9178863f
Bryan Truwant <bryan.truwant@gmail.com>
2021-06-02 00:12:03 +0200

    Code de la classe : World

--- src/main/java/com/herocorp/game/World.java
+++ src/main/java/com/herocorp/game/World.java
@@ -1,5 +1,19 @@
 package com.herocorp.game;
 
 public class World {
+
+    private int temps = 0;
+
+    public World () {
+
+    }
+
+    public int getTemps () {
+        return this.temps;
+    }
+
+    public void setTemps (int newTemps) {
+        this.temps = newTemps;
+    }
     
 }
