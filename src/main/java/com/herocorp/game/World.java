4478b88a2592c54d040a6cf31bb12610520eec63
Bryan Truwant <bryan.truwant@gmail.com>
2021-06-02 00:22:13 +0200

    Completion du code : World

--- src/main/java/com/herocorp/game/World.java
+++ src/main/java/com/herocorp/game/World.java
@@ -1,11 +1,23 @@
 package com.herocorp.game;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import com.herocorp.metier.acteurs.Chasseur;
+import com.herocorp.metier.lieux.AbstractLieu;
+import com.herocorp.metier.lieux.Donjon;
+import com.herocorp.metier.lieux.Guilde;
+
 public class World {
 
     private int temps = 0;
+    private ArrayList <Chasseur> listeChasseurs = new ArrayList <Chasseur> ();
+    private ArrayList <Donjon> listeDonjons = new ArrayList <Donjon> ();
+    private ArrayList <Guilde> listeGuildes = new ArrayList <Guilde> ();
+    private HashMap <String, AbstractLieu> mapLieux = new HashMap <String, AbstractLieu> ();
 
     public World () {
-
+        // TODO : Générer les lieux, les stocker dans mapLieux, générer des donjons et la population initiale
     }
 
     public int getTemps () {
@@ -14,6 +26,22 @@ public class World {
 
     public void setTemps (int newTemps) {
         this.temps = newTemps;
+    }
+
+    public ArrayList <Chasseur> getListeChasseurs () {
+        return this.listeChasseurs;
+    }
+
+    public ArrayList <Donjon> getListeDonjons () {
+        return this.listeDonjons;
+    }
+
+    public ArrayList <Guilde> getListeGuildes () {
+        return this.listeGuildes;
+    }
+
+    public HashMap <String, AbstractLieu> getMapLieux () {
+        return this.mapLieux;
     }
     
 }
