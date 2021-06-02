31e780442c6b71c17fda11d6a184370ee29066f9
alex.goussian@gmail.com <alex.goussian@gmail.com>
2021-06-02 19:32:52 +0200

    Tests Donjon OK

--- src/test/java/com/herocorp/metier/lieux/DonjonTest.java
+++ src/test/java/com/herocorp/metier/lieux/DonjonTest.java
@@ -1,6 +1,13 @@
 package com.herocorp.metier.lieux;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+import com.herocorp.metier.acteurs.Chasseur;
+import com.herocorp.metier.acteurs.Monstre;
+import com.herocorp.metier.groupes.GroupeMonstres;
+import com.herocorp.metier.groupes.GroupeRaid;
+import com.herocorp.tools.Classe;
+import com.herocorp.tools.Coord;
 
 import org.junit.Test;
 
@@ -9,13 +16,34 @@ import org.junit.Test;
  */
 public class DonjonTest
 {
-    /**
-     * Rigorous Test :-)
-     */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testGroupeMonstre() {
+        // INIT
+        GroupeMonstres attendu = new GroupeMonstres();
+        attendu.add(new Monstre("Souli", Classe.S, attendu));
+        attendu.add(new Monstre("Mailine", Classe.A, attendu));
+        attendu.add(new Monstre("Bryan", Classe.A, attendu));
+        Donjon donjon = new Donjon(new Coord(0, 0));
+        donjon.setGroupeMonstre(attendu);
+        // RES
+        GroupeMonstres resultat = donjon.getGroupeMonstres();
+        // TEST
+        assertEquals(attendu, resultat);
+    }
+
+    @Test
+    public void testGroupeRaid() {
+        // INIT
+        GroupeRaid attendu = new GroupeRaid();
+        attendu.add(new Chasseur("Mailine"));
+        attendu.add(new Chasseur("Bryan"));
+        attendu.add(new Chasseur("Alex"));
+        Donjon donjon = new Donjon(new Coord(0, 0));
+        donjon.setGroupeRaid(attendu);
+        // RES
+        GroupeRaid resultat = donjon.getGroupeRaid();
+        // TEST
+        assertEquals(attendu, resultat);
     }
 }
 
