cea579a4e1af963bd60c320166df63e79d2f5db5
MailineN <nguyen.mailine@gmail.com>
2021-06-02 09:57:09 +0200

    Not Tested GrpChasseurs & GrpRaid Tests

--- src/test/java/com/herocorp/metier/groupes/GroupeChasseursTest.java
+++ src/test/java/com/herocorp/metier/groupes/GroupeChasseursTest.java
@@ -1,8 +1,10 @@
 package com.herocorp.metier.groupes;
+package com.herocorp.metier.acteurs;
 
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+
 
 /**
  * Unit test for simple App.
@@ -13,9 +15,22 @@ public class GroupeChasseursTest
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
+    public void testGetTaille()
     {
-        assertTrue( true );
+        // INIT
+        Chasseur chasseur1 = new Chasseur("Pouf");
+        Chasseur chasseur2 = new Chasseur("Pif");
+        GroupeChasseurs groupe = new GroupeChasseurs(); 
+
+        groupe.add(chasseur1); 
+        groupe.add(chasseur2); 
+
+        // RES
+        Int resultat = groupe.getTaille(); 
+
+        // TEST 
+        assertEquals(2,resultat);
+
     }
 }
 
--- src/test/java/com/herocorp/metier/groupes/GroupeRaidTest.java
+++ src/test/java/com/herocorp/metier/groupes/GroupeRaidTest.java
@@ -1,4 +1,5 @@
 package com.herocorp.metier.groupes;
+package com.herocorp.metier.acteurs;
 
 import static org.junit.Assert.assertTrue;
 
@@ -9,13 +10,53 @@ import org.junit.Test;
  */
 public class GroupeRaidTest
 {
-    /**
+    /**d
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
+    public void testIsPlein()
     {
-        assertTrue( true );
+        // INIT
+        Chasseur chasseur1 = new Chasseur("Pouf");
+        Chasseur chasseur2 = new Chasseur("Pif");
+        Chasseur chasseur3 = new Chasseur("Miam");
+        Chasseur chasseur4 = new Chasseur("Bipou");
+        Chasseur chasseur5 = new Chasseur("TotoroDepressif");
+        Chasseur chasseur6 = new Chasseur("Po");
+        Chasseur chasseur7 = new Chasseur("Bou");
+        Chasseur chasseur8 = new Chasseur("J'ai");
+        Chasseur chasseur9 = new Chasseur("Plus");
+        Chasseur chasseur10 = new Chasseur("D'idées");
+        GroupeRaid groupe1 = new GroupeChasseurs(); 
+        GroupeRaid groupe2 = new GroupeChasseurs(); 
+
+        groupe1.add(chasseur1); 
+        groupe1.add(chasseur2); 
+        groupe1.add(chasseur3); 
+        groupe1.add(chasseur4); 
+        groupe1.add(chasseur5); 
+        groupe1.add(chasseur6); 
+        groupe1.add(chasseur7); 
+        groupe1.add(chasseur8); 
+        groupe1.add(chasseur9); 
+        groupe1.add(chasseur10); 
+
+
+        groupe2.add(chasseur1); 
+        groupe2.add(chasseur2); 
+        groupe2.add(chasseur3); 
+        groupe2.add(chasseur4); 
+        groupe2.add(chasseur5); 
+        groupe2.add(chasseur6); 
+        groupe2.add(chasseur7); 
+
+        // RES 
+        resultat1 = groupe1.isPlein();
+        resultat2 = groupe2.isPlein();
+        // TEST 
+        assertTrue(resultat1);
+        assertFalse(resultat2)
+
     }
 }
 
