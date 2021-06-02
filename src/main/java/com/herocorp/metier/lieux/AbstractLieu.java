0f4bcee348a6a2905dc9b24f49778a1b7686b5b7
alex.goussian@gmail.com <alex.goussian@gmail.com>
2021-06-02 16:47:51 +0200

    Ajout des coordonnées pour les lieux

--- src/main/java/com/herocorp/metier/lieux/AbstractLieu.java
+++ src/main/java/com/herocorp/metier/lieux/AbstractLieu.java
@@ -1,18 +1,22 @@
 package com.herocorp.metier.lieux;
 
 import com.herocorp.metier.groupes.AbstractGroupe;
+import com.herocorp.tools.Coord;
+
 
 public class AbstractLieu {
     
     private static int dernierId = 0;
     private int id;
     private String nom;
+    private Coord coord;
     private AbstractGroupe habitants;
 
-    public AbstractLieu (String nom) {
+    public AbstractLieu (String nom, Coord coord) {
         this.id = AbstractLieu.dernierId;
         AbstractLieu.dernierId += 1;
         this.nom = nom;
+        this.coord = coord;
     }
 
     public int getId () {
@@ -21,6 +25,10 @@ public class AbstractLieu {
 
     public String getNom () {
         return this.nom;
+    }
+
+    public Coord getPosition(){
+        return this.coord;
     }
 
     public AbstractGroupe getHabitants () {
