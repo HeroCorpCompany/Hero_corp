8c10633bc0e9610efe1628af3526de2b27f7b473
Bryan Truwant <bryan.truwant@gmail.com>
2021-06-03 00:39:10 +0200

    Suppression des tests contenant de l'aléatoire

--- src/test/java/com/herocorp/metier/groupes/GroupeMonstresTest.java
+++ src/test/java/com/herocorp/metier/groupes/GroupeMonstresTest.java
@@ -1,6 +1,5 @@
 package com.herocorp.metier.groupes;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.util.Random;
@@ -18,55 +17,6 @@ public class GroupeMonstresTest
     /**
      * Rigorous Test :-)
      */
-    @Test
-    public void testCombatForce()
-    {
-        GroupeMonstres groupe = new GroupeMonstres();
-        Monstre Monstre1 = new Monstre("Pouf", Classe.A, groupe);
-        Monstre Monstre2 = new Monstre("Pif", Classe.A, groupe);
-        Monstre Monstre3 = new Monstre("Souli", Classe.A, groupe);
-        Monstre Monstre4 = new Monstre("Bipou", Classe.A, groupe);
-        Monstre Monstre5 = new Monstre("TotoroDepressif", Classe.A, groupe);
-        Monstre Monstre6 = new Monstre("Po", Classe.A, groupe);
-        Monstre Monstre7 = new Monstre("Bou", Classe.A, groupe);
-        Monstre Monstre8 = new Monstre("Maymay", Classe.A, groupe);
-        Monstre Monstre9 = new Monstre("Bribri", Classe.A, groupe);
-        Monstre Monstre10 = new Monstre("Alexandrie", Classe.A, groupe);
-        GroupeMonstres groupe1 = new GroupeMonstres();
-
-        Monstre1.setClasse(Classe.A); //4
-        Monstre2.setClasse(Classe.B); //3
-        Monstre3.setClasse(Classe.D); //1
-        Monstre4.setClasse(Classe.A); //4
-        Monstre5.setClasse(Classe.B); //3
-        Monstre6.setClasse(Classe.S); //5
-        Monstre7.setClasse(Classe.A); //4
-        Monstre8.setClasse(Classe.A); //4
-        Monstre9.setClasse(Classe.S); //5
-        Monstre10.setClasse(Classe.D); //1
-
-        groupe1.add(Monstre1); 
-        groupe1.add(Monstre2); 
-        groupe1.add(Monstre3); 
-        groupe1.add(Monstre4); 
-        groupe1.add(Monstre5); 
-        groupe1.add(Monstre6); 
-        groupe1.add(Monstre7); 
-        groupe1.add(Monstre8); 
-        groupe1.add(Monstre9); 
-        groupe1.add(Monstre10); 
-
-        Random randommo = new Random(); 
-        randommo.setSeed(20);
-        double attendu = 34*randommo.nextGaussian();
-
-        // RES
-        double force = groupe1.combatForce(); 
-
-        boolean resultat = attendu == force;
-
-        // TEST
-        assertTrue(resultat);;
-    }
+    
 }
 
--- src/test/java/com/herocorp/metier/groupes/GroupeRaidTest.java
+++ src/test/java/com/herocorp/metier/groupes/GroupeRaidTest.java
@@ -1,11 +1,9 @@
 package com.herocorp.metier.groupes;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import com.herocorp.metier.acteurs.Chasseur;
-import com.herocorp.tools.Classe;
 
 import org.junit.Test;
 
@@ -63,49 +61,5 @@ public class GroupeRaidTest
 
     }
 
-    public void testCombatForce(){
-        Chasseur chasseur1 = new Chasseur("Pouf");
-        Chasseur chasseur2 = new Chasseur("Pif");
-        Chasseur chasseur3 = new Chasseur("Souli");
-        Chasseur chasseur4 = new Chasseur("Bipou");
-        Chasseur chasseur5 = new Chasseur("TotoroDepressif");
-        Chasseur chasseur6 = new Chasseur("Po");
-        Chasseur chasseur7 = new Chasseur("Bou");
-        Chasseur chasseur8 = new Chasseur("Maymay");
-        Chasseur chasseur9 = new Chasseur("Bribri");
-        Chasseur chasseur10 = new Chasseur("Alexandrie");
-        GroupeRaid groupe1 = new GroupeRaid(); 
-        GroupeRaid groupe2 = new GroupeRaid(); 
-
-        chasseur1.setClasse(Classe.A); //4
-        chasseur2.setClasse(Classe.B); //3
-        chasseur3.setClasse(Classe.D); //1
-        chasseur4.setClasse(Classe.A); //4
-        chasseur5.setClasse(Classe.B); //3
-        chasseur6.setClasse(Classe.S); //5
-        chasseur7.setClasse(Classe.A); //4
-        chasseur8.setClasse(Classe.A); //4
-        chasseur9.setClasse(Classe.S); //5
-        chasseur10.setClasse(Classe.D); //1
-
-        groupe1.add(chasseur1); 
-        groupe1.add(chasseur2); 
-        groupe1.add(chasseur3); 
-        groupe1.add(chasseur4); 
-        groupe1.add(chasseur5); 
-        groupe1.add(chasseur6); 
-        groupe1.add(chasseur7); 
-        groupe1.add(chasseur8); 
-        groupe1.add(chasseur9); 
-        groupe1.add(chasseur10); 
-
-        int attendu = 34;
-
-        // RES
-        int resultat = groupe1.combatForce(); 
-
-        // TEST
-        assertEquals(resultat,attendu);
-    }
 }
 
