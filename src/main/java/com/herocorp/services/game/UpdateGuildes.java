16c5ab22d44e905d407e5db04e2a197ccc138165
alex.goussian@gmail.com <alex.goussian@gmail.com>
2021-06-03 03:35:18 +0200

    Arbre décision Guilde

--- src/main/java/com/herocorp/services/game/UpdateGuildes.java
+++ src/main/java/com/herocorp/services/game/UpdateGuildes.java
@@ -1,11 +1,36 @@
 package com.herocorp.services.game;
 
+import java.util.ArrayList;
+
 import com.herocorp.game.World;
+import com.herocorp.metier.lieux.Guilde;
+import com.herocorp.services.metier.lieux.GuildeService;
 
 
 public class UpdateGuildes {
     
     public static void updateGuildes ( World world ) {
-        // TODO : voir arbre guildes
+        ArrayList<Guilde> listeGuildes = world.getListeGuildes();
+        for (int i = 0; i < listeGuildes.size(); i++) {
+            updateGuild(world, listeGuildes.get(i));
+        }
+    }
+
+    public static void updateGuild ( World world, Guilde guilde ) {
+        if (guilde.getArgent() < 0) {
+            recrutement(world, guilde);
+        }
+        else {
+            world.supprimerGuilde(guilde);
+        }
+    }
+
+    public static void recrutement ( World world, Guilde guilde ) {
+        if (guilde.getArgent() - (2*GuildeService.montantAPayer(guilde) + 5000) >= 0) {
+            guilde.setRecrute(true);
+        }
+        else {
+            guilde.setRecrute(false);
+        }
     }
 }
--- src/main/java/com/herocorp/services/metier/lieux/GuildeService.java
+++ src/main/java/com/herocorp/services/metier/lieux/GuildeService.java
@@ -15,11 +15,20 @@ public class GuildeService {
 
     public static void payerSalaires (Guilde guilde) {
         int size = guilde.getTaille();
-
         for(int i = 0; i < size; i++){
             int salaire = ((Chasseur) guilde.getMembres().get(i)).getSalaire();
             guilde.setArgent(guilde.getArgent() - salaire);
             ((Chasseur) guilde.getMembres().get(i)).agmenterArgent(salaire);
         }
+    }
+
+    public static int montantAPayer (Guilde guilde) {
+        int size = guilde.getTaille();
+        int montant = 0;
+        for(int i = 0; i < size; i++){
+            int salaire = ((Chasseur) guilde.getMembres().get(i)).getSalaire();
+            montant += salaire;
+        }
+        return montant;
     }
 }
