66648ed88d092f109db6daf88b1206bb8d92b09d
alex.goussian@gmail.com <alex.goussian@gmail.com>
2021-06-03 04:40:55 +0200

    Correction code DonjonService génération de monstres

--- src/main/java/com/herocorp/services/metier/lieux/DonjonService.java
+++ src/main/java/com/herocorp/services/metier/lieux/DonjonService.java
@@ -1,43 +1,17 @@
 package com.herocorp.services.metier.lieux;
 
-import java.util.Random;
-
 import com.herocorp.metier.acteurs.Monstre;
+import com.herocorp.services.metier.acteurs.MonstreService;
 import com.herocorp.metier.groupes.GroupeMonstres;
 import com.herocorp.metier.lieux.Donjon;
-import com.herocorp.tools.Classe;
 
 public class DonjonService {
 
     public static void remplirDonjon (Donjon donjon) {
         GroupeMonstres groupeMonstres = new GroupeMonstres();
-        Classe classe;
-        Random random = new Random();
         
         for (int i = 0; i < 10; i++){
-            switch (random.nextInt(5)) {
-                case 0:
-                    classe = Classe.S;
-                    break;
-
-                case 1:
-                    classe = Classe.A;
-                    break;
-
-                case 2:
-                    classe = Classe.B;
-                    break;
-
-                case 3:
-                    classe = Classe.C;
-                    break;
-            
-                default:
-                    classe = Classe.D;
-                    break;
-            }
-
-            Monstre monstre = new Monstre("Vilain", classe, groupeMonstres);
+            Monstre monstre = MonstreService.genererMonstre(groupeMonstres);
             groupeMonstres.add(monstre);
         }
     
