6e4b50268ed8a3f60c609b1cf7c0e10d83c6ac46
Bryan Truwant <bryan.truwant@gmail.com>
2021-06-03 23:14:31 +0200

    GroupeChasseursTest : testCombatForce

--- src/test/java/com/herocorp/metier/groupes/GroupeChasseursTest.java
+++ src/test/java/com/herocorp/metier/groupes/GroupeChasseursTest.java
@@ -3,6 +3,7 @@ package com.herocorp.metier.groupes;
 import static org.junit.Assert.assertEquals;
 
 import com.herocorp.metier.acteurs.Chasseur;
+import com.herocorp.services.metier.acteurs.ChasseurService;
 
 import org.junit.Test;
 
@@ -32,6 +33,24 @@ public class GroupeChasseursTest
         // TEST 
         assertEquals(2,resultat);
 
+    }
+
+    @Test
+    public void testCombatForce () {
+        // INIT
+        GroupeChasseurs groupe = new GroupeChasseurs();
+        int forceTotale = 0;
+        for (int i = 0; i < 10; i++) {
+            Chasseur chasseur = new Chasseur("Souli");
+            ChasseurService.attribuerClasse(chasseur);
+            forceTotale += chasseur.getForce();
+            groupe.add(chasseur);
+        }
+        int attendu = forceTotale / groupe.getTaille();
+        // RES
+        int resultat = groupe.combatForce();
+        // TEST
+        assertEquals(attendu, resultat);
     }
 
     
