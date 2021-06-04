55cdaaf258a8595e5eb40b6cba79df3d02a5df69
alex.goussian@gmail.com <alex.goussian@gmail.com>
2021-06-04 20:06:32 +0200

    Implémentation de la boucle de jeu

--- src/main/java/com/herocorp/game/Game.java
+++ src/main/java/com/herocorp/game/Game.java
@@ -1,5 +1,58 @@
 package com.herocorp.game;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import com.herocorp.metier.acteurs.Chasseur;
+import com.herocorp.metier.lieux.AbstractLieu;
+import com.herocorp.metier.lieux.Donjon;
+import com.herocorp.metier.lieux.Forum;
+import com.herocorp.metier.lieux.Guilde;
+import com.herocorp.services.game.WorldService;
+import com.herocorp.services.metier.acteurs.ChasseurService;
+import com.herocorp.services.metier.lieux.DonjonService;
+import com.herocorp.tools.Coord;
+
 public class Game {
-    
+    private World world;
+    private int nbIteration;
+
+    public Game (int nbIteration) {
+        ArrayList <Chasseur> listeChasseurs = new ArrayList<>();
+        ArrayList <Donjon> listeDonjons = new ArrayList<>();
+        ArrayList <Guilde> listeGuildes = new ArrayList<>();
+        HashMap <String, AbstractLieu> mapLieux = new HashMap<>();
+        Forum forum = new Forum(new Coord(0, 0));
+        mapLieux.put("Forum", forum);
+        for (int i = 0; i < 5; i++) {
+            Donjon donjon = new Donjon(new Coord(0, 0));
+            DonjonService.remplirDonjon(donjon);
+            listeDonjons.add(donjon);
+        }
+        for (int i = 0; i < 90; i++) {
+            Chasseur chasseur = new Chasseur("Souli");
+            ChasseurService.attribuerClasse(chasseur);
+            ChasseurService.changerLieu(chasseur, forum);
+            listeChasseurs.add(chasseur);
+        }
+
+        this.world = new World(listeChasseurs, listeDonjons, listeGuildes, mapLieux);
+        this.nbIteration = nbIteration;
+    }
+
+    public void run (){
+        for (int i = 0; i < this.nbIteration; i++) {
+            update();
+            render();
+        }
+    }
+
+    public void update () {
+        WorldService.updateWorld(this.world);
+        WorldService.updateTemps(this.world);
+    }
+
+    public void render () {
+        System.out.println(this.world);
+    }
 }
