873e8bb104fe03bb98c12e4322e54fbe6c552061
MailineN <nguyen.mailine@gmail.com>
2021-06-04 21:46:40 +0200

    testVerifierDj, AllerDj, Ajouter/Retirer Chasseur

--- src/main/java/com/herocorp/services/metier/groupes/GroupeRaidService.java
+++ src/main/java/com/herocorp/services/metier/groupes/GroupeRaidService.java
@@ -2,13 +2,14 @@ package com.herocorp.services.metier.groupes;
 
 import com.herocorp.metier.acteurs.Chasseur;
 import com.herocorp.metier.groupes.GroupeRaid;
+import com.herocorp.metier.groupes.GroupeMonstres;
 import com.herocorp.metier.lieux.Donjon;
 
 public class GroupeRaidService {
     
     public static boolean verifierGroupe (GroupeRaid groupe) {
         // TODO : vérifier la taille du groupe
-        return groupe.getTaille() == 10;
+        return (groupe.getTaille() == 10);
     }
 
     public static void allerDonjon (GroupeRaid groupe, Donjon donjon) {
@@ -16,8 +17,13 @@ public class GroupeRaidService {
         groupe.setPosition(donjon);
     }
 
-    public static void attaquerDonjon (GroupeRaid groupe) {
+    public static boolean attaquerDonjon (GroupeRaid groupe) {
         // TODO : renvoyer vrai si la force du groupe est supérieure à celle du donjon, faux sinon
+        double forceMoyChasseur = Double.valueOf(groupe.combatForce()); 
+        GroupeMonstres grpMonstre = groupe.getCible().getGroupeMonstres();
+        double forceMoyMonstres = grpMonstre.combatForce();
+
+        return(forceMoyChasseur > forceMoyMonstres);
     }
 
     public static void ajouterChasseur (GroupeRaid groupe, Chasseur chasseur) {
--- src/test/java/com/herocorp/services/metier/groupes/GroupeRaidServiceTest.java
+++ src/test/java/com/herocorp/services/metier/groupes/GroupeRaidServiceTest.java
@@ -1,7 +1,11 @@
 package com.herocorp.services.metier.groupes;
+import com.herocorp.metier.acteurs.Chasseur;
+import com.herocorp.metier.groupes.GroupeRaid;
+import com.herocorp.metier.lieux.*;
+import com.herocorp.tools.Coord;
 
-import static org.junit.Assert.assertTrue;
 
+import static org.junit.Assert.*;
 import org.junit.Test;
 
 /**
@@ -13,9 +17,166 @@ public class GroupeRaidServiceTest
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
+    public void testVerifierGroupe() 
     {
-        assertTrue( true );
+        // INIT
+        Chasseur chasseur1 = new Chasseur("Pouf");
+        Chasseur chasseur2 = new Chasseur("Pif");
+        Chasseur chasseur3 = new Chasseur("Souli");
+        Chasseur chasseur4 = new Chasseur("Bipou");
+        Chasseur chasseur5 = new Chasseur("TotoroDepressif");
+        Chasseur chasseur6 = new Chasseur("Po");
+        Chasseur chasseur7 = new Chasseur("Bou");
+        Chasseur chasseur8 = new Chasseur("Maymay");
+        Chasseur chasseur9 = new Chasseur("Bribri");
+        Chasseur chasseur10 = new Chasseur("Alexandrie");
+        GroupeRaid groupe1 = new GroupeRaid(); 
+        GroupeRaid groupe2 = new GroupeRaid(); 
+
+        groupe1.add(chasseur1); 
+        groupe1.add(chasseur2); 
+        groupe1.add(chasseur3); 
+        groupe1.add(chasseur4); 
+        groupe1.add(chasseur5); 
+        groupe1.add(chasseur6); 
+        groupe1.add(chasseur7); 
+        groupe1.add(chasseur8); 
+        groupe1.add(chasseur9); 
+        groupe1.add(chasseur10); 
+
+
+        groupe2.add(chasseur1); 
+        groupe2.add(chasseur2); 
+        groupe2.add(chasseur3); 
+        groupe2.add(chasseur4); 
+        groupe2.add(chasseur5); 
+        groupe2.add(chasseur6); 
+        groupe2.add(chasseur7); 
+
+        // RES 
+        boolean resultat1 = GroupeRaidService.verifierGroupe(groupe1);
+        boolean resultat2 = GroupeRaidService.verifierGroupe(groupe2);
+        // TEST 
+        System.out.println(resultat2);
+        assertTrue(resultat1);
+        assertFalse(resultat2); 
+        //Le test passe pas avec assertFalse et je ne comprends pas pourquoi
+
     }
+
+    @Test
+    public void testAllerDonjon() 
+    {
+        // INIT
+        Chasseur chasseur1 = new Chasseur("Pouf");
+        Chasseur chasseur2 = new Chasseur("Pif");
+        Chasseur chasseur3 = new Chasseur("Souli");
+        Chasseur chasseur4 = new Chasseur("Bipou");
+        Chasseur chasseur5 = new Chasseur("TotoroDepressif");
+        Chasseur chasseur6 = new Chasseur("Po");
+        Chasseur chasseur7 = new Chasseur("Bou");
+        Chasseur chasseur8 = new Chasseur("Maymay");
+        Chasseur chasseur9 = new Chasseur("Bribri");
+        Chasseur chasseur10 = new Chasseur("Alexandrie");
+        GroupeRaid groupe1 = new GroupeRaid(); 
+
+        groupe1.add(chasseur1); 
+        groupe1.add(chasseur2); 
+        groupe1.add(chasseur3); 
+        groupe1.add(chasseur4); 
+        groupe1.add(chasseur5); 
+        groupe1.add(chasseur6); 
+        groupe1.add(chasseur7); 
+        groupe1.add(chasseur8); 
+        groupe1.add(chasseur9); 
+        groupe1.add(chasseur10); 
+
+        Donjon donjon = new Donjon(new Coord(0, 0));
+
+        // RES 
+        GroupeRaidService.allerDonjon(groupe1,donjon);
+
+        AbstractLieu resultat = groupe1.getPosition();
+        // TEST 
+        System.out.println(resultat);
+        assertEquals(resultat,donjon);
+       
+    }
+    
+    @Test
+    public void testAjouterChasseur() 
+    {
+        // INIT
+        Chasseur chasseur1 = new Chasseur("Pouf");
+        Chasseur chasseur2 = new Chasseur("Pif");
+        Chasseur chasseur3 = new Chasseur("Souli");
+        Chasseur chasseur4 = new Chasseur("Bipou");
+        Chasseur chasseur5 = new Chasseur("TotoroDepressif");
+        Chasseur chasseur6 = new Chasseur("Po");
+        Chasseur chasseur7 = new Chasseur("Bou");
+        Chasseur chasseur8 = new Chasseur("Maymay");
+        Chasseur chasseur9 = new Chasseur("Bribri");
+        Chasseur chasseur10 = new Chasseur("Alexandrie");
+        GroupeRaid groupe1 = new GroupeRaid(); 
+
+        groupe1.add(chasseur1); 
+        groupe1.add(chasseur2); 
+        groupe1.add(chasseur3); 
+        groupe1.add(chasseur4); 
+        groupe1.add(chasseur5); 
+        groupe1.add(chasseur6); 
+        groupe1.add(chasseur7); 
+        groupe1.add(chasseur8); 
+        groupe1.add(chasseur9); 
+
+        int attendu = 10;
+
+        // RES 
+        GroupeRaidService.ajouterChasseur(groupe1, chasseur10);
+
+        int resultat = groupe1.getTaille();
+        // TEST 
+        assertEquals(resultat,attendu);
+       
+    }
+
+    @Test
+    public void testRetirerChasseur() 
+    {
+        // INIT
+        Chasseur chasseur1 = new Chasseur("Pouf");
+        Chasseur chasseur2 = new Chasseur("Pif");
+        Chasseur chasseur3 = new Chasseur("Souli");
+        Chasseur chasseur4 = new Chasseur("Bipou");
+        Chasseur chasseur5 = new Chasseur("TotoroDepressif");
+        Chasseur chasseur6 = new Chasseur("Po");
+        Chasseur chasseur7 = new Chasseur("Bou");
+        Chasseur chasseur8 = new Chasseur("Maymay");
+        Chasseur chasseur9 = new Chasseur("Bribri");
+        Chasseur chasseur10 = new Chasseur("Alexandrie");
+        GroupeRaid groupe1 = new GroupeRaid(); 
+
+        groupe1.add(chasseur1); 
+        groupe1.add(chasseur2); 
+        groupe1.add(chasseur3); 
+        groupe1.add(chasseur4); 
+        groupe1.add(chasseur5); 
+        groupe1.add(chasseur6); 
+        groupe1.add(chasseur7); 
+        groupe1.add(chasseur8); 
+        groupe1.add(chasseur9); 
+        groupe1.add(chasseur10);
+
+        int attendu = 9;
+
+        // RES 
+        GroupeRaidService.retirerChasseur(groupe1, chasseur10);
+
+        int resultat = groupe1.getTaille();
+        // TEST 
+        assertEquals(resultat,attendu);
+       
+    }
+    
 }
 
