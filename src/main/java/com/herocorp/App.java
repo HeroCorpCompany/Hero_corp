3b08ee8a0974249b08df06d94824fef680ba13c1
Bryan Truwant <bryan.truwant@gmail.com>
2021-06-07 15:44:41 +0200

    Nombre de tours : 2

--- src/main/java/com/herocorp/App.java
+++ src/main/java/com/herocorp/App.java
@@ -7,10 +7,6 @@ public class App
     public static void main( String[] args )
     {
         Game game = new Game();
-<<<<<<< HEAD
         game.run(2);
-=======
-        game.run(300);
->>>>>>> f_Ameliorer_Probas
     }
 }
