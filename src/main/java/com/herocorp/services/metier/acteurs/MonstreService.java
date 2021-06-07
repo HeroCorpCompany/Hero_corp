04cb9163f3c3b49a8b72e1860b35ea65d1ab7541
Bryan Truwant <bryan.truwant@gmail.com>
2021-06-07 15:19:14 +0200

    Ajout des probas sur le niveau du monstre

--- src/main/java/com/herocorp/services/metier/acteurs/MonstreService.java
+++ src/main/java/com/herocorp/services/metier/acteurs/MonstreService.java
@@ -11,26 +11,20 @@ public class MonstreService {
     public static Monstre genererMonstre (GroupeMonstres groupe) {
         Classe classe;
         Random rnd = new Random();
-        int randomInt = rnd.nextInt(5);
-        switch (randomInt) {
-            case 0:
-                classe = Classe.D;
-                break;
-            case 1:
-                classe = Classe.C;
-                break;
-            case 2:
-                classe = Classe.B;
-                break;
-            case 3:
-                classe = Classe.A;
-                break;
-            case 4:
-                classe = Classe.S;
-                break;
-            default:
-                classe = Classe.D;
+        int randomInt = rnd.nextInt(100);
+
+        if (randomInt < 40) {
+            classe = Classe.D;
+        } else if (randomInt < 30) {
+            classe = Classe.C;
+        } else if (randomInt < 20) {
+            classe = Classe.B;
+        } else if (randomInt < 99) {
+            classe = Classe.A;
+        } else {
+            classe = Classe.S;
         }
+
         Monstre monstre = new Monstre("TestMonstre", classe, groupe);
         return monstre;
     }
